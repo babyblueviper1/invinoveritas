b6413b7e8b1ba6610cc6f50e5bc477cdb868ca82
babyblueviper1 <babyblueviperbusiness@gmail.com>
2026-04-07 16:30:59 -0400

    Update config.py . nodes eternal

--- config.py
+++ config.py
@@ -18,6 +18,10 @@ if not NODE_URL:
           "Please set the NODE_URL environment variable to your Lightning bridge address "
           "(e.g. http://your-vps-ip:8081)")
 
+
+NOSTR_PRIVATE_KEY = os.getenv("NOSTR_PRIVATE_KEY")  # hex format, optional but recommended
+
+
 # ======================
 # Pricing Configuration
 # ======================
